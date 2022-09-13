bd4478c075f784a9abd452f9c2ed8273525c81bf
BESTWISHES <BESTWISHES@DESKTOP-8OGSDKS>
2022-09-13 19:11:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace changer
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            var money = 0.0;
+            var curs = 60;
+            Console.WriteLine("Курс: "+curs);
+            Console.WriteLine("Введите деньги которые нужно перевести в доллары ");
+           money= Convert.ToDouble(Console.ReadLine());
+            if (money < 500)
+            {
+                money -= 8;
+            } 
+                 else
+            {
+                money*=0.37;
+                Console.WriteLine(money);
+            }    
+            
+            
+            
+            money *= curs;
+            Console.WriteLine("Вы получите: "+money);
+        }
+    }
+}
